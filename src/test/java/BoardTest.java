d33f524b44387d3f3d1a5d96738921808959c79e
adrian <wi25b064@technikum-wien.at>
2026-07-01 23:27:41 +0200

    Added test root and empty template files

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    void getCells() {
+    }
+
+    @Test
+    void isCellEmpty() {
+    }
+
+    @Test
+    void place() {
+    }
+
+    @Test
+    void print() {
+    }
+
+    @Test
+    void checkGameState() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
