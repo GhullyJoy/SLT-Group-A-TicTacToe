a90fafd23f2cc1628edbc486faff4c210119dd4b
adrian <wi25b064@technikum-wien.at>
2026-07-01 23:47:05 +0200

    Working on testfiles, committing because next comit will be modifying pom.xml to add a dependency were missing to add tapSystemOut

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -10,10 +10,36 @@ class BoardTest {
 
     @Test
     void isCellEmpty() {
+        Board positiveBoard = new Board();
+        positiveBoard.place(0,0,'x');
+        assertFalse(positiveBoard.isCellEmpty(0,0),"Zelle wurde nicht korrekt befüllt");
+
+        Board negativeBoard = new Board();
+        assertThrows(
+                IllegalArgumentException.class
+                ,()->{
+                    negativeBoard.isCellEmpty(3,3);
+                }
+                ,"Wirft keine Exception wenn invalider Input gegeben wurde"
+        );
     }
 
     @Test
     void place() {
+
+        //erster test eher redundant, weil er von isCellEmpty abhängt
+        Board positiveBoard = new Board();
+        positiveBoard.place(0,0,'x');
+        assertFalse(positiveBoard.isCellEmpty(0,0),"Zelle wurde nicht korrekt befüllt");
+
+        Board negativeBoard = new Board();
+        assertThrows(
+                IllegalArgumentException.class
+                ,()->{
+                    negativeBoard.place(3,3,'x');
+                }
+                ,"Wirft keine Exception wenn invalider Input gegeben wurde"
+        );
     }
 
     @Test
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    @Test
+    void getMarker() {
+        Player positivePlayer = new Player('x');
+        assertEquals('x',positivePlayer.getMarker(),"Marker falsch gesetzt");
 
+        //kein negativer testfall möglich, da man weder einen leeren char, noch 2 chars übergeben kann
+    }
 }
